100b09146eb16534cdda91cebb4aa1be1c5cd881
Ivan Labozhevich <en2wyy@gmail.com>
2023-02-08 22:57:46 +0300

    Upgrade eslint and stylelint

--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['stylelint-config-standard-scss'],
-  plugins: ['stylelint-scss', 'stylelint-order'],
-  rules: {
-    'order/order': ['custom-properties', 'declarations'],
-    'order/properties-alphabetical-order': true,
-    'selector-class-pattern': null,
-  },
-};
