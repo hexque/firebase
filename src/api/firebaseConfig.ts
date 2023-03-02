7949c2f0d67efe34b1b7bfcfa12ec3764129f5e7
Ivan Labozhevich <en2wyy@gmail.com>
2023-03-02 22:07:32 +0300

    Upgrade configuration firebase

--- src/api/firebaseConfig.ts
+++ src/api/firebaseConfig.ts
@@ -1,6 +1,7 @@
 // libraries
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,3 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(app);
+
+export const auth = getAuth(app);
