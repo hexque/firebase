61a88de00fc89d546d47f9ba539f1963aa587edf
Ivan Labozhevich <en2wyy@gmail.com>
2023-03-04 17:45:57 +0300

    Upgrade 404 page not found

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,12 +1,9 @@
 // libraries
 import React, { FC, PropsWithChildren } from 'react';
-// components
-import Header from 'components/Header';
 
 const Wrapper: FC<PropsWithChildren> = ({ children }) => (
   <div className="wrapper">
-    <Header />
-    <div className="content-wrapper">
+    <div className="wrapper-container">
       {children}
     </div>
   </div>
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,11 @@
 const NotFound: React.FC = () => (
-  <div>
-    <h3>Not Found</h3>
+  <div className="not-found">
+    <h3 className="not-found-message">Oops! Page not found</h3>
+    <h1 className="not-found-code">
+      <span>4</span>
+      <span>0</span>
+      <span>4</span>
+    </h1>
   </div>
 );
 
